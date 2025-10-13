d2bbd3e3ff233c299c798290204a65a053ac9c4e
Felipe <felipecezaralmeida0@gmail.com>
2025-10-13 09:51:20 -0300

    Rota criada... mais tragédia

---
+++ Back-End/api.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+import funcao
+
+#Rodar FastAPI = python -m uvicorn api:app --reload
+
+#Testar as rotas no fastapi
+# /docs > Documentação Swagger
+# /redoc > Documentação redoc
+
+app = FastAPI(title= "Gerenciador de Filmes")
+
+#GET > Pegar/Listar
+#POST > Enviar/Cadastar
+#PUT > Atualizar
+#DELETE > Deletar
+
+#API sempre retorna dados em JSON (Chave: Valor)
+@app.get("/")
+def home():
+    return {"Mensagem":"Bom dia... quero café!!"}
