a51d806f991f4691758fc4ca207793d7ba78010f
Felipe <felipecezaralmeida0@gmail.com>
2025-10-14 09:55:04 -0300

    Aqui eu já posso adicionar filmes! (Arrasei)

--- Front-End/app.py
+++ Front-End/app.py
@@ -23,3 +23,17 @@ if menu == "Catálogo":
                 st.info("Nenhum filme encontrado")
         else:
             st.error("Erro ao conectar com a API")
+
+elif menu == "Adicionar Filme":
+    st.subheader("➕ Adicionar Filme")
+    titulo = st.text_input("Título do Filme")
+    genero = st.text_input("Gênero")
+    ano = st.number_input("Ano de Lançamento", min_value=1900, max_value=2100)
+    avaliacao = st.number_input("Avaliação de (0 a 10)", min_value=0, max_value=10, step=1)
+    if st.button("Salvar filme"):
+     params = {"titulo": titulo, "genero": genero, "ano": ano, "avaliacao": avaliacao}
+     response = requests.post(f"{API_URL}/filmes", params=params)
+     if response.status_code == 200:
+        st.success("Filme adicionado com sucesso!")
+    else:
+        st.error("Erro ao adicionar o filme")
