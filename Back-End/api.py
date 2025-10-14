c2f4e44c406418c5d8aa2f780764f60b51752825
Felipe <felipecezaralmeida0@gmail.com>
2025-10-14 11:25:09 -0300

    Agr conseguimos atualizar, o front  venceu!

--- Back-End/api.py
+++ Back-End/api.py
@@ -27,3 +27,18 @@ def catalogo():
     for filme in filmes:
         lista.append({ "id": filme[0], "título": filme[1], "gênero": filme[2], "ano": filme[3], "avaliação": filme[4]})
     return {"filmes": lista}
+
+@app.post("/filmes")
+def adicionar_filme(titulo: str, genero: str, ano: int, avaliacao: float):
+    funcao.criar_filme(titulo, genero, ano, avaliacao)
+    return {"mensagem": "Filme adicionado com sucesso!"}
+
+@app.put("/filmes/{id_filme}")
+def atualizar_filme(id_filme:int,nova_avaliacao: float):
+    filmes = funcao.listar_movies()
+    if filmes:
+        funcao.atualizar_movies(id_filme, nova_avaliacao)
+        return{"mensagem": "Filme atualizado com sucesso!"}
+    else:
+        return{"erro":"Filme não encontrado"}
+
--- Front-End/app.py
+++ Front-End/app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Escolhi a Diversão", page_icon="🎭")
 st.title("🎨 Gerenciador de Filmes")
 
 #Menu lateral sidebar
-menu = st.sidebar.radio("Menu", ["Catálogo"])
+menu = st.sidebar.radio("Menu", ["Catálogo","Adicionar Filme", "Atualizar Filme"])
 
 if menu == "Catálogo":
     st.subheader("Todos os filmes 🎬")
@@ -19,21 +19,41 @@ if menu == "Catálogo":
         if filmes:
             for filme in filmes:
                 st.write(f"{filme['título']} ( {filme['ano']})  -  {filme['gênero']}  -  🔍 {filme['avaliação']}")
-            else:
-                st.info("Nenhum filme encontrado")
         else:
-            st.error("Erro ao conectar com a API")
-
-elif menu == "Adicionar Filme":
-    st.subheader("➕ Adicionar Filme")
-    titulo = st.text_input("Título do Filme")
-    genero = st.text_input("Gênero")
-    ano = st.number_input("Ano de Lançamento", min_value=1900, max_value=2100)
-    avaliacao = st.number_input("Avaliação de (0 a 10)", min_value=0, max_value=10, step=1)
-    if st.button("Salvar filme"):
-     params = {"titulo": titulo, "genero": genero, "ano": ano, "avaliacao": avaliacao}
-     response = requests.post(f"{API_URL}/filmes", params=params)
-     if response.status_code == 200:
-        st.success("Filme adicionado com sucesso!")
+            st.info("Nenhum filme encontrado")
     else:
-        st.error("Erro ao adicionar o filme")
+       st.error("Erro ao conectar com a API")
+
+elif menu == "Adicionar filme":
+    st.subheader("Adicionar Filme ao catálogo")
+    titulo = st.text_input("Título do filme")
+    genero = st.text_input("Gênero")
+    ano = st.number_input("Ano de lançamento", min_value=0, max_value=10)
+    avaliacao = st.number_input("Avaliação (0 à 10)", min_value=0.0, max_value=10.0, step=1)
+
+    if st.button("Salvar"):
+        params = {"titulo": titulo,
+                  "genero": genero,
+                  "ano": ano,
+                  "avaliacao": avaliacao}
+        response = requests.post(f"{API_URL}/filmes",params=params)
+        if response.status_code == 200:
+            st.success("FIlme adicionado com sucesso")
+        else:
+            st.error("Erro ao adicionar o filme")
+
+elif menu == "Atualizar Filme":
+    st.subheader("Atualizar Filme")
+    id_filme = st.number_input("ID do filme a atualizar", min_value=1, step=1)
+    nova_avaliacao = st.number_input("Nova avaliação", min_value=0.0, max_value=10.0, step=0.1)
+    if st.button("Atualizar"):
+        dados = {"nova_avaliacao":nova_avaliacao}
+        response = requests.put(f"{API_URL}/filmes{id_filme}", params=dados)
+        if response.status_code == 200:
+            data = response.json()
+            if "erro" in data:
+                st.warning(data["erro"])
+            else:
+                st.success("Filme atualizado com sucesso!")
+        else:
+            st.error("Erro no seu código... lascado")
